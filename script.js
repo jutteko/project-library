3980b4ae15f0e52d8b7e12aa5f912c4bff034aa8
jutteko <jurgen.francois@gmail.com>
2026-02-24 13:56:51 +0100

    toggle van formulier werkt niet

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+// een voorlopige lijst maken van boeken
 myLibrary = [];
 
 const boek1 = new Book(
@@ -20,6 +21,15 @@ myLibrary.push(boek1);
 myLibrary.push(boek2);
 myLibrary.push(boek3);
 
+// formulier wel en niet tonen
+const formNewBook = document.querySelector(".frm-new-book");
+const btnShowForm = document.querySelector(".btn-add");
+btnShowForm.addEventListener("click", (e) => {
+  formNewBook.classList.toggle("show-hide-form");
+  console.log("hallo");
+});
+
+// De funtie voor het samenstellen van de cards
 function displayBooks() {
   const cardContainer = document.querySelector(".card-container");
   cardContainer.innerHTML = "";
@@ -47,6 +57,7 @@ function displayBooks() {
     btnRead.classList.add("btn-read");
     btnRead.textContent = book.read ? "Read" : "Not read";
     card.appendChild(btnRead);
+    // toggle tussen gelezen en niet geleren
     btnRead.addEventListener("click", (e) => {
       myLibrary[index].read = !myLibrary[index].read;
       displayBooks();
@@ -54,14 +65,16 @@ function displayBooks() {
     const btnDelete = document.createElement("button");
     btnDelete.classList.add("btn-delete");
     btnDelete.textContent = "Delete";
+    // deleten van een boekencard
     btnDelete.addEventListener("click", (e) => {
       myLibrary.splice(index, 1);
       displayBooks();
     });
     card.appendChild(btnDelete);
-    //
+    // de samengesteld card toevoegen aan de div cardcontainer
     cardContainer.append(card);
   });
 }
 
+// na het laden van de pagina de cards tonen
 displayBooks();
